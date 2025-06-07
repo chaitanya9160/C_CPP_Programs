8b382723a07ce5c3906e40d3f3d51bb25b85e3d3
Chaitanya Patil <162734569+chaitanya9160@users.noreply.github.com>
2025-06-07 20:35:49 +0530

    Add files via upload

---
+++ Practice1/four.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    if(!-5)   //(!-5)= 0 that is false
+    {
+        printf("good");
+    }
+    else{
+        printf("bad");
+    }
+    getch();
+
+}
